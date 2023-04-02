aa85992041c8deea93dec4121f12290f2af21f1e
ruivalente99 <rui.valente99@gmail.com>
2023-04-02 22:59:03 +0100

    Add work experience

--- src/screens/Portfolio/index.tsx
+++ src/screens/Portfolio/index.tsx
@@ -1,15 +1,43 @@
 import PortfolioItem from "./PortfolioItem";
 import neo from "assets/images/neo.png";
+import openvia from "assets/images/openvia.svg";
 const portfolioData = [
   {
     id: 1,
+    image: openvia,
+    title: "Software Engineer - Openvia",
+    link: "openvia.io",
+    period: {
+      start: "2022",
+      end: "Present"
+    },
+    description: `As a Frontend Engineer at Openvia, I have been responsible for designing, creating, and maintaining architecture
+    for various projects using React and Typescript, from the initial commit until deployment. I also connect
+    frontends with GraphQL or REST APIs, create views using Bootstrap and customize styling for Openvia.
+    Additionally, I have maintained legacy projects using Ionic, Angular, Vanilla JS, HTML, CSS, and Ajax. I work
+    using an agile methodology and check code quality with Sonarqube and Jest to prevent bugs and ensure code
+    coverage. My skills include working with Jenkins, Docker, and AWS to keep projects live, as well as developing
+    projects from scratch. `
+  },
+  {
+    id: 2,
     image: neo,
     title: "Software Engineer - Neoception",
-    link: "#0",
-    description:
-      "Working on a project for Shop Floor management. The project is built on React with Material UI, Redux, Javascript and Jira for project management.",
+    period: {
+      start: "2021",
+      end: "2022"
+    },
+    link: "neocetion.com",
+    description: ` As a Junior Software Engineer, I gained experience working on a web-based project using React and JavaScript.
+    I became proficient in unit testing, code reviews, and utilizing React component libraries like Material
+    UI. I was responsible for creating new features and attending meetings with project clients using Jira tickets
+    on a Kanban board. Through this, I developed software engineering and communication skills, while
+    sharpening my English language proficiency. This experience exposed me to the professional world of software
+    development, preparing me for future roles as a software engineer. I have utilized my skills and knowledge
+    gained to contribute to the success of numerous projects.`
+    ,
   },
-  
+
 ];
 
 const Portfolio = () => {
