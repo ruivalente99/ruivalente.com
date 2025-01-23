fc612e1cad115687a43e75d1596ab9f9969afa9a
Rui  Valente <rui.valente99@gmail.com>
2025-01-23 17:06:49 +0000

    Update nextconfig

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withMDX = require('@next/mdx')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
